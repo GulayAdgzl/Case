8d75c8169e796db505ae5b7e36719dfdd30373fe
Gülay ADIGÜZEL <44726684+GulayAdgzl@users.noreply.github.com>
2022-10-27 16:05:57 +0300

    Update card_screen.dart

--- Case/lib/features/BkMobil/view/card_screen.dart
+++ Case/lib/features/BkMobil/view/card_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_full_learn/features/BkMobil/service/person_network_service.dart';
+import 'package:provider/provider.dart';
 
 import '../Const/Project_color.dart';
 import '../Const/Project_text.dart';
 import '../model/person_model.dart';
+import '../viewModel/Button_viewModel.dart';
 
 class CardScreen extends StatefulWidget {
   @override
@@ -12,62 +14,62 @@ class CardScreen extends StatefulWidget {
 
 class _CardScreenState extends State<CardScreen> {
   final PersonNetworkService personService = PersonNetworkService();
+  late final ButtonViewModel _buttonViewModel;
+  @override
+  void initState() {
+    super.initState();
+    _buttonViewModel = ButtonViewModel();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-            child: SafeArea(
-                child: FutureBuilder(
-                    future: personService.fetchPersons(1),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<List<PersonModel>> snapshot) {
-                      if (snapshot.hasData) {
-                        return Row(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                child: Card(
-                                  color: ColorsItems.black.withOpacity(0.5),
-                                  child: ListView.builder(
-                                      itemCount: snapshot.data?.length,
-                                      itemBuilder:
-                                          (BuildContext context, int index) {
-                                        var currentPerson =
-                                            snapshot.data![index];
-                                        return _CardDetail(currentPerson);
-                                      }),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      }
-                      if (snapshot.hasError) {
-                        return const Center(
-                            child: Icon(
-                          Icons.error,
-                          color: ColorsItems.red,
-                          size: 82.0,
-                        ));
-                      }
-                      return Center(child: _loadingColumn());
-                    }))));
+    return ChangeNotifierProvider.value(
+        value: _buttonViewModel,
+        builder: (context, child) {
+          return Scaffold(
+              appBar: AppBar(),
+              body: Container(
+                  child: SafeArea(
+                      child: FutureBuilder(
+                          future: personService.fetchPersons(1),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<List<PersonModel>> snapshot) {
+                            if (snapshot.hasData) {
+                              return Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      child: Card(
+                                        color:
+                                            Color.fromARGB(255, 107, 102, 102)
+                                                .withOpacity(0.5),
+                                        child: ListView.builder(
+                                            itemCount: snapshot.data?.length,
+                                            itemBuilder: (BuildContext context,
+                                                int index) {
+                                              var currentPerson =
+                                                  snapshot.data![index];
+                                              return _CardDetail(currentPerson);
+                                            }),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }
+                            if (snapshot.hasError) {
+                              return const Center(
+                                  child: Icon(
+                                Icons.error,
+                                color: ColorsItems.red,
+                                size: 82.0,
+                              ));
+                            }
+                            return Center(child: _loadingWidget());
+                          }))));
+        });
   }
 
-  Column _loadingColumn() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: const <Widget>[
-        CircularProgressIndicator(),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text(StringText.loading)
-      ],
-    );
-  }
 
   Card _CardDetail(PersonModel currentPerson) {
     return Card(
@@ -84,5 +86,13 @@ class _CardScreenState extends State<CardScreen> {
         ],
       ),
     );
+  }
+
+  Widget _loadingWidget() {
+    return Consumer<ButtonViewModel>(builder: (context, value, child) {
+      return value.isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SizedBox();
+    });
   }
 }
